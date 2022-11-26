9d56f374827ebefccae321c04de31cdf15d9fe1c
SikroYang <yxx95lz@163.com>
2022-11-26 11:28:44 +0800

    2022年11月26日11:28:41

--- Vue.Lidao/nuxt.config.js
+++ Vue.Lidao/nuxt.config.js
@@ -62,7 +62,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://www.lidaoisland.com',
+    baseURL: 'https://www.lidaoisland.com',
+    // baseURL: 'http://192.168.123.2:5222',
     // headers: { 'Content-Type': 'application/json', 'crossDomain': true },
     // withCredentials: true
   },
