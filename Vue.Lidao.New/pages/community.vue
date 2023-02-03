2d61acc34b8d4ba958e229b1b1f26ac144bd699f
SikroYang <yxx95lz@163.com>
2023-02-03 16:20:39 +0800

    2023年2月3日16:20:34

--- Vue.Lidao.New/pages/community.vue
+++ Vue.Lidao.New/pages/community.vue
@@ -2,7 +2,7 @@
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2022-10-24 15:15:45
  * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2023-02-03 16:07:23
+ * @LastEditTime: 2023-02-03 16:19:35
  * @FilePath: \project\pages\login.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -39,7 +39,7 @@
           <div style="padding: 14px">
             <span
               @click="clickRadio(o)"
-              style="font-size: 15px; font-weight: 700"
+              style="font-size: 15px; font-weight: 700;cursor: pointer;"
               >{{ o.Name | filterAmount(6) }}</span
             >
             <div class="bottom clearfix">
@@ -97,7 +97,7 @@
           <div style="padding: 14px">
             <span
               @click="clickRadio(o)"
-              style="font-size: 15px; font-weight: 700"
+              style="font-size: 15px; font-weight: 700;cursor: pointer;"
               >{{ o.Name | filterAmount(6) }}</span
             >
             <div class="bottom clearfix">
