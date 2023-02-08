38351cdaa718b2fdd4b0e75afb5997b839ad67e3
SikroYang <yxx95lz@163.com>
2023-02-08 12:40:43 +0800

    2023年2月8日12:40:39

--- Vue.Lidao.New/nuxt.config.js
+++ Vue.Lidao.New/nuxt.config.js
@@ -2,7 +2,7 @@
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2022-11-04 14:31:55
  * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2022-12-03 10:15:34
+ * @LastEditTime: 2023-02-02 10:33:38
  * @FilePath: \qundao\qundao\nuxt.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -42,7 +42,6 @@ export default {
     // { src: '@/plugins/route.js', ssr: false },
     { src: "@/plugins/vue-quill-editor.js", ssr: false },
     { src: '@plugins/lib-flexible.js', ssr: false },
-
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
