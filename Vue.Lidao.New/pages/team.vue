98875e9444f7b7bbebf8699474f93190a83282ca
SikroYang <yxx95lz@163.com>
2023-05-11 10:53:17 +0800

    2023年5月11日10:53:12

--- Vue.Lidao.New/pages/team.vue
+++ Vue.Lidao.New/pages/team.vue
@@ -7,117 +7,124 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-    <div class="bac">
-      <Top />
-      <div class="content">
-        <div style="padding-left: 40px;">
-          <p class="about" style="font-weight: bold;">关于我们</p>
+  <div class="bac">
+    <Top />
+    <div class="content">
+      <div style="padding-left: 40px;">
+        <p class="about" style="font-weight: bold;">关于我们</p>
         <p class="about" style="color: rgba(0, 0, 0, 0.5);">ABOUT US</p>
-        </div>
-        <div class="introductio">
-            <ul>
-                <li class="introduction_li" v-for="(item,i) in list" :key="i">
-                    <img class="pic" :src="'https://www.lidaoisland.com/'+item.PhotoUrl" alt="" srcset="">
-                    <p style="color: black;font-size: 24px;">{{ item.Name }}</p>
-                    <!-- <p style="font-size: 16px;">Knowledge is power</p> -->
-                    <div style="white-space: pre;font-size: 12px;">{{ item.Description }}</div>
-                    <!-- <div>
+      </div>
+      <div class="introductio">
+        <ul>
+          <li class="introduction_li" v-for="(item, i) in list" :key="i">
+            <img class="pic" :src="'https://www.lidaoisland.com/' + item.PhotoUrl" alt="" srcset="">
+            <p style="color: black;font-size: 24px;">{{ item.Name }}</p>
+            <!-- <p style="font-size: 16px;">Knowledge is power</p> -->
+            <div style="white-space: pre;font-size: 12px;">{{ item.Description }}</div>
+            <!-- <div>
                         <img class="in_img" src="~assets/instagram.png" alt="" srcset="">
                         <img class="in_img" src="~assets/twitter.png" alt="" srcset="">
                         <img class="in_img" src="~assets/facebook.png" alt="" srcset="">
                     </div> -->
-                </li>
-               
-            </ul>
-        </div>
-        
+          </li>
+
+        </ul>
       </div>
-      <div class="map"></div>
+
     </div>
-  </template>
-  
-  <script>
-  import Top from "../components/Header.vue";
-  export default {
-    name: "TeamPage",
-    components: { Top },
-    data: function () {
+    <div class="map"></div>
+  </div>
+</template>
+
+<script>
+import Top from "../components/Header.vue";
+export default {
+  name: "TeamPage",
+  components: { Top },
+  data: function () {
     return {
       list: [],
     };
   },
-    methods: {},
-    mounted(){
-      this.$axios.post("/Api/TeamMember/List", {
-        headers: {
-          'Content-Type': "application/json;charset=UTF-8",
-        },
-      }).then((res) => {
-        console.log(res.data.Data.List);
-        this.list=res.data.Data.List
-      });
-    }
-    
-  };
-  </script>
-  <style scoped>
-  .bac {
+  methods: {},
+  mounted() {
+    this.$axios.post("/Api/TeamMember/List", {
+      headers: {
+        'Content-Type': "application/json;charset=UTF-8",
+      },
+    }).then((res) => {
+      console.log(res.data.Data.List);
+      this.list = res.data.Data.List
+    });
+  }
+
+};
+</script>
+<style scoped>
+.bac {
   background: radial-gradient(66.85% 40.65% at 15.86% 11.82%, #FFE0B0 0%, #CEFFFB 91.92%), #FFFFFF;
   height: 2477px;
   position: relative;
 }
-  h1,
-  h3 {
-    color: #9f7861;
-  }
-  .flex {
-    display: flex;
-  }
- .content{
-    width: 70%;
-    margin: 0 auto;
-    padding-top: 40px;
-   
- }
 
-.introductio{
-    padding-top: 40px;
-    width: 100%;
-    color: #9f7861;
+h1,
+h3 {
+  color: #9f7861;
 }
-.introductio ul{
+
+.flex {
+  display: flex;
+}
+
+.content {
+  width: 70%;
+  margin: 0 auto;
+  padding-top: 40px;
+
+}
+
+.introductio {
+  padding-top: 40px;
+  width: 100%;
+  color: #9f7861;
+}
+
+.introductio ul {
   width: 100%;
   display: flex;
   flex-flow: wrap;
 }
-.introductio ul li{
-    list-style: none;
-    width: 200px;
-    height: 450px;
-    margin-right: 66px;
-    text-align: left;
+
+.introductio ul li {
+  list-style: none;
+  width: 20%;
+  height: 450px;
+  margin-right: 66px;
+  text-align: left;
 }
-.pic{
-width: 200px;
-height: 260px;
+
+.pic {
+  width: 200px;
+  height: 260px;
 }
-.in_img{
-    width: 30px;
-    height: 30px;
-    margin-right: 5px;
+
+.in_img {
+  width: 30px;
+  height: 30px;
+  margin-right: 5px;
 }
-.about{
+
+.about {
   font-weight: 400;
-font-size: 30px;
-line-height: 15px;
+  font-size: 30px;
+  line-height: 15px;
 }
-.map{
+
+.map {
   background: url(../assets/map/mapbase.png) no-repeat;
   background-size: 100%;
   width: 100%;
   height: 1100px;
   position: absolute;
   bottom: 0;
-}
-  </style>
-  
+}</style>
